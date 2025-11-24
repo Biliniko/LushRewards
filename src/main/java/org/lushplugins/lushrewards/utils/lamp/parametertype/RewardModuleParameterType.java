01b6cf91d93a83179362f105e84ccc68abacafa0
OakLoaf <oak@beaconstudios.org>
2025-11-24 10:13:32 +0000

    Added permission check to reward module tab-completion

--- src/main/java/org/lushplugins/lushrewards/utils/lamp/parametertype/RewardModuleParameterType.java
+++ src/main/java/org/lushplugins/lushrewards/utils/lamp/parametertype/RewardModuleParameterType.java
@@ -1,26 +1,31 @@
 package org.lushplugins.lushrewards.utils.lamp.parametertype;
 
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.lushplugins.lushrewards.LushRewards;
 import org.lushplugins.lushrewards.reward.module.RewardModule;
 import revxrsal.commands.autocomplete.SuggestionProvider;
-import revxrsal.commands.command.CommandActor;
+import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 import revxrsal.commands.node.ExecutionContext;
 import revxrsal.commands.parameter.ParameterType;
 import revxrsal.commands.stream.MutableStringStream;
 
-public class RewardModuleParameterType implements ParameterType<CommandActor, RewardModule> {
+public class RewardModuleParameterType implements ParameterType<BukkitCommandActor, RewardModule> {
 
     @Override
-    public RewardModule parse(@NotNull MutableStringStream input, @NotNull ExecutionContext<CommandActor> context) {
+    public RewardModule parse(@NotNull MutableStringStream input, @NotNull ExecutionContext<BukkitCommandActor> context) {
         return LushRewards.getInstance().getRewardModuleManager().getModule(input.source());
     }
 
     @Override
-    public @NotNull SuggestionProvider<CommandActor> defaultSuggestions() {
-        // TODO: Add permission check to only tab-complete modules that the user has permission for
-        return (context) -> LushRewards.getInstance().getRewardModuleManager().getModules().stream()
-            .map(RewardModule::getId)
-            .toList();
+    public @NotNull SuggestionProvider<BukkitCommandActor> defaultSuggestions() {
+
+        return (context) -> {
+            Player player = context.actor().asPlayer();
+            return LushRewards.getInstance().getRewardModuleManager().getModules().stream()
+                .map(RewardModule::getId)
+                .filter(id -> player == null || player.hasPermission("lushrewards.use.%s".formatted(id)))
+                .toList();
+        };
     }
 }
